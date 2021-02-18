0edd667f4d81f744d112384823c43f81ee9dac65
aditya <adityafernanda2013@gmail.com>
2021-02-18 23:31:04 +0700

    React-BWA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const category = "SMARTPHONE";
+  const price = 15000000;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="Parentbox">
+      <div class="Foto">
+        <img src="iphone12remake.jpg"/>
+      </div>
+      <div class="Deskripsi">
+        <p class="Cate">{category}</p>
+        <h1 class="Title">iPhone 12 Pro/128Gb</h1>
+        <p class="Price">IDR {price}</p>
+        <p class="Info">
+        iOS adalah sistem operasi seluler yang dibuat dan dikembangkan oleh Apple Inc. khusus untuk perangkat kerasnya. Ini adalah sistem operasi yang saat ini memberdayakan banyak perangkat seluler perusahaan, termasuk iPhone, dan iPod Touch; itu juga mendukung iPad sebelum pengenalan iPadOS pada 2019.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
